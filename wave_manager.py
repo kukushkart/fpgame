9534a45fc0f5114bbf6253446ac532cd77bb5b97
Raman Ponya <poniavinroman@gmail.com>
2025-07-11 01:44:09 +0300

    Какие-то незначительные изменения

--- wave_manager.py
+++ wave_manager.py
@@ -2,33 +2,33 @@ import pygame
 
 WAVES_CONFIG = {
     1: [
-        {"zombie_count": 4, "delay": 0},
+        {"zombie_count": 4, "delay": 5},
         {"zombie_count": 4, "delay": 5}
     ],
     2: [
-        {"zombie_count": 4, "delay": 0},
+        {"zombie_count": 4, "delay": 5},
         {"zombie_count": 6, "delay": 5},
         {"zombie_count": 6, "delay": 5}
     ],
     3: [
-        {"zombie_count": 6, "delay": 0},
         {"zombie_count": 6, "delay": 5},
-        {"zombie_count": 8, "delay": 5},
-        {"zombie_count": 10, "delay": 3},
-        {"zombie_count": 10, "delay": 3}
+        {"zombie_count": 6, "delay": 3},
+        {"zombie_count": 8, "delay": 3},
+        {"zombie_count": 10, "delay": 5},
+        {"zombie_count": 10, "delay": 5}
     ],
     4: [
-        {"zombie_count": 8, "delay": 0},
-        {"zombie_count": 9, "delay": 5},
-        {"zombie_count": 5, "delay": 3},
         {"zombie_count": 8, "delay": 3},
+        {"zombie_count": 9, "delay": 3},
+        {"zombie_count": 5, "delay": 1},
+        {"zombie_count": 8, "delay": 5},
         {"zombie_count": 14, "delay": 7}
     ],
     5: [
-        {"zombie_count": 10, "delay": 0},
         {"zombie_count": 10, "delay": 5},
-        {"zombie_count": 8, "delay": 5},
-        {"zombie_count": 6, "delay": 5},
-        {"zombie_count": 20, "delay": 5}
+        {"zombie_count": 10, "delay": 5},
+        {"zombie_count": 8, "delay": 3},
+        {"zombie_count": 6, "delay": 3},
+        {"zombie_count": 20, "delay": 6}
     ]
 }
